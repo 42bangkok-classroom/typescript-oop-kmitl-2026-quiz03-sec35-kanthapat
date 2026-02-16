dfd77c87f530c4431436d527e69e9235835064f0
kanthapat <sailom.01216@gmail.com>
2026-02-16 09:37:54 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,23 @@
-export class Person {}
+export class Person {
+    firstname?: string;
+    lastname?: string;
+    age: number = 0;
+    static COUNTRY: string = 'Thailand';
+
+    constructor(firstname?: string, lastname?: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    setAge(age:number) {
+        this.age = age;
+    }
+
+    getAge() {
+        console.log(this.age);
+    }
+
+    getFullName() {
+        console.log(`${this.firstname} ${this.lastname}`)
+    }
+}
